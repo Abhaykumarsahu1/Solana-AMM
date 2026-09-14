520a25b6d5b8f04e1615c5b1c9426760451e0307
Abhay Kumar Sahu <aks.abhaykumarsahu@gmail.com>
2026-09-14 18:19:06 +0000

    State implementation done

--- programs/amm-program/src/state.rs
+++ programs/amm-program/src/state.rs
@@ -2,4 +2,12 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-
+pub struct AmmConfig{
+    pub authority: Option<PubKey>,
+    pub token_a: Pubkey,
+    pub token_b: Pubkey,
+    pub lp_mint: Pubkey,
+    pub locked : bool,
+    pub fee: u64,
+    pub bump: u8,
+}

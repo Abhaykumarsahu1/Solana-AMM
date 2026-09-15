3a7ebd9c1c2af80698d25b1e3848266e63064f6c
Abhay Kumar Sahu <aks.abhaykumarsahu@gmail.com>
2026-09-15 17:04:47 +0000

    completed implementation of AMM intitialization

--- programs/amm-program/src/instructions/initialize.rs
+++ programs/amm-program/src/instructions/initialize.rs
@@ -1,1 +1,77 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount}
+};
+use crate::AmmConfig;
+
+#[derive(Accounts)]
+#[instruction(seed:u64)]
+struct initialize<'info>{
+
+    #[account(mut)] //lamports deduction
+    pub payer : Signer<'info>, //i am the one who will create the amm pool
+
+    //these are the mint account will represent 2 tokens their metadata  
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
+
+    #[account(
+        init,
+        payer=payer,
+        associated_token::mint = mint_x,
+        associated_token::authority = config, //amm pool will control the vault not me
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+    
+    #[account(
+        init,
+        payer=payer,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer=payer,
+        seeds = [b"lp", config.key().as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = config,
+    )]
+    pub lp_mint : Account<'info, Mint>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + AmmConfig::INIT_SPACE,
+        seeds = [b"amm_config", payer.key().as_ref()],
+        bump
+    )]
+    pub config : Account<'info, AmmConfig>, //initializing the config/metadata for AMMpool
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handle_intialize(
+    ctx: Context<initialize>,
+    seed:u64,
+    fee: u16,
+)->Result<()>{
+
+    let config = &mut ctx.accounts.config;//targeting the config accounts present in the struct intialize
+
+    config.seed = seed;
+    config.authority = Some(ctx.accounts.payer.key());
+    config.mint_y = ctx.accounts.mint_y.key();
+    config.mint_x = ctx.accounts.mint_x.key();
+    config.fee = fee;
+    config.locked = false;
+    config.bump = ctx.bumps.config;
+    config.lp_mint = ctx.accounts.lp_mint.key();
+
+    Ok(())
+}
--- programs/amm-program/src/state.rs
+++ programs/amm-program/src/state.rs
@@ -3,10 +3,11 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct AmmConfig{
+    pub seed : u64,
     pub authority: Option<PubKey>,
-    pub token_a: Pubkey,
-    pub token_b: Pubkey,
-    pub lp_mint: Pubkey,
+    pub mint_y: Pubkey,
+    pub mint_x: Pubkey,
+    pub lp_mint: Pubkey, //represent my ownership share of the pool
     pub locked : bool,
     pub fee: u64,
     pub bump: u8,
